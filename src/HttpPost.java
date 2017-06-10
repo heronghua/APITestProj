e7307763c091776d030b1961ac6a289c63381db6
heronghua <heronghua@heronghua-V5>
2017-06-10 23:44:40 +0800

    heronghua--简化参数 让命令行可以方便调用 跟curl 的post 调用方法及其相似

--- src/HttpPost.java
+++ src/HttpPost.java
@@ -3,54 +3,49 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
 
-public class BaseHttpPostWokerThread extends Thread {
+public class HttpPost extends Thread {
 	
 	private String url;
 	
-	private HashMap<String,String> params;
+	private String params;
 	
-	public BaseHttpPostWokerThread(String url,HashMap<String, String> params) {
+	public HttpPost(String url,String params) {
 		this.url=url;
 		this.params = params;
+	}
+	
+	public static void main(String[] args) {
+		HttpPost postWokerThread=new HttpPost(args[0], args[1]);
+		postWokerThread.start();
 	}
 	
 	@Override
 	public void run() {
 		URL url;
+		HttpURLConnection httpUrlConnection = null;
 		try {
 			url = new URL(this.url);
-			HttpURLConnection httpUrlConnection = (HttpURLConnection) url.openConnection();
+			httpUrlConnection = (HttpURLConnection) url.openConnection();
 			httpUrlConnection.setRequestMethod("POST");
 			httpUrlConnection.setDoInput(true);
 			httpUrlConnection.setDoOutput(true);
 			
-			httpUrlConnection.getOutputStream().write(getParamsBytes(this.params));
+			httpUrlConnection.getOutputStream().write(this.params.getBytes());
 			
 			BufferedReader br = new BufferedReader(new InputStreamReader(httpUrlConnection.getInputStream(), "utf-8"));
 			StringBuffer sb = new StringBuffer();
 			String line=null;
 			while ((line=br.readLine())!=null) {
-				sb.append(line).append("\n\r");
+				sb.append(line).append("\n");
 			}
 			System.out.println(sb.toString());
 		} catch (IOException e) {
 			e.printStackTrace();
+		}finally{
+			httpUrlConnection.disconnect();
 		}
 		
-		
 	}
-	
-	private byte[] getParamsBytes(HashMap<String, String> params) {
-		StringBuffer sb = new StringBuffer();
-		
-		for (String v:params.keySet()) {
-			sb.append("&").append(v).append("=").append(params.get(v));
-		}
-		
-		return sb.toString().substring(1).getBytes();
-	}
-
 	
 }
--- src/Post.java
+++
@@ -1,6 +0,0 @@
-
-public class Post {
-	public static void main(String[] args) {
-		
-	}
-}
